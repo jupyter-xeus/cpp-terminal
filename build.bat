b9430ae053462d25f44dbe93bc9c9b50d84a9fa7
Ondřej Čertík <ondrej@certik.us>
2019-09-24 16:55:58 -0600

    Build all examples

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 cl -I. /EHsc examples/kilo.cpp /Fe:kilo.exe
+cl -I. /EHsc examples/menu.cpp /Fe:menu.exe
+cl -I. /EHsc examples/keys.cpp /Fe:keys.exe
+cl -I. /EHsc examples/colors.cpp /Fe:colors.exe
