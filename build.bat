6d31dddaf573922a3d8ec7656df30a1a4be925ba
Ondřej Čertík <ondrej@certik.us>
2019-09-24 16:53:57 -0600

    Include directories

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /EHsc examples/kilo.cpp /Fe:kilo.exe
+cl -I. /EHsc examples/kilo.cpp /Fe:kilo.exe
