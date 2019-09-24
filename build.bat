30e29b104c5da095af95790537a8223459e2324f
Ondřej Čertík <ondrej@certik.us>
2019-09-24 16:52:11 -0600

    Update Windows build

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /EHsc t.cpp /Fe:t.exe
-cl /EHsc term.cpp /Fe:term.exe
+cl /EHsc examples/kilo.cpp /Fe:kilo.exe
