fe655fa4e9ebed03cc2983e0334435179f9e4384
Ondřej Čertík <ondrej@certik.us>
2019-09-25 09:58:30 -0600

    Add a tests.sh script for executing tests

---
+++ tests.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -ex
+
+./colors
+
+echo "Expected to succeed:"
+./colors < README.md
+echo "Expected to fail"
+set +e
+./keys < README.md
+if [[ $? != 0 ]]; then
+  echo "Success"
+else
+  echo "Fail"
+  exit 1
+fi
+set -e
+
+echo
+echo "Tests passed."
