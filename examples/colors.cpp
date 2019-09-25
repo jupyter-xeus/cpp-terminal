ca72afec1022fb780cff3029db73ead8d7b60035
Ondřej Čertík <ondrej@certik.us>
2019-09-25 09:54:22 -0600

    Simplify colors.cpp

--- examples/colors.cpp
+++ examples/colors.cpp
@@ -1,45 +1,10 @@
 #include "terminal.h"
 
 using Term::Terminal;
-using Term::cursor_on;
-using Term::cursor_off;
-using Term::move_cursor;
 using Term::color;
 using Term::fg;
 using Term::bg;
 using Term::style;
-using Term::Key;
-
-void render(const Terminal &term, int rows, int cols, int pos)
-{
-    std::string scr;
-    scr.reserve(16*1024);
-
-    scr.append(cursor_off());
-    scr.append(move_cursor(1, 1));
-
-    for (int i=1; i <= rows; i++) {
-        if (i == pos) {
-            scr.append(color(fg::red));
-            scr.append(color(bg::gray));
-            scr.append(color(style::bold));
-        } else {
-            scr.append(color(fg::blue));
-            scr.append(color(bg::green));
-        }
-        scr.append(std::to_string(i) + ": item");
-        scr.append(color(bg::reset));
-        scr.append(color(fg::reset));
-        scr.append(color(style::reset));
-        if (i < rows) scr.append("\n");
-    }
-
-    scr.append(move_cursor(rows / 2, cols / 2));
-
-    scr.append(cursor_on());
-
-    term.write(scr);
-}
 
 int main() {
     try {
