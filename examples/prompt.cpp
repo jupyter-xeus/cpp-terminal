8072622612c2878f90d3951d6b690926c9eaf93f
Ondřej Čertík <ondrej@certik.us>
2019-09-26 10:20:56 -0600

    Refactor the code, introduce Model

--- examples/prompt.cpp
+++ examples/prompt.cpp
@@ -4,14 +4,22 @@ using Term::Terminal;
 using Term::Key;
 using Term::move_cursor;
 
-std::string render(int row, int cols, const std::string &prompt_string,
-        const std::string &input) {
-    int col = prompt_string.size() + input.size() + 1;
-    std::string out = move_cursor(row, 1) + prompt_string + input;
-    for (size_t i=0; i < cols-out.size(); i++) {
+// This model contains all the information about the state of the prompt in an
+// abstract way, irrespective of where or how it is rendered.
+struct Model {
+    std::string prompt_string; // The string to show as the prompt
+    std::string input; // The current input string in the prompt
+    // The current cursor position in the "input" string, starting from (1,1)
+    int cursor_col, cursor_row;
+};
+
+std::string render(const Model &m, int prompt_row, int term_cols) {
+    int col = m.prompt_string.size() + m.input.size() + 1;
+    std::string out = move_cursor(prompt_row, 1) + m.prompt_string + m.input;
+    for (size_t i=0; i < term_cols-out.size(); i++) {
         out.append(" ");
     }
-    out.append(move_cursor(row, col));
+    out.append(move_cursor(prompt_row, col));
     return out;
 }
 
@@ -20,32 +28,37 @@ std::string prompt(const Terminal &term, const std::string &prompt_string) {
     term.get_cursor_position(row, col);
     int rows, cols;
     term.get_term_size(rows, cols);
-    std::string input, s;
+
+
+    Model m;
+    m.prompt_string = prompt_string;
+    m.cursor_col = 1;
+
     int key;
-    std::cout << render(row, cols, prompt_string, input) << std::flush;
+    std::cout << render(m, row, cols) << std::flush;
     while ((key = term.read_key()) != Key::ENTER) {
         if (  (key >= 'a' && key <= 'z') ||
               (key >= 'A' && key <= 'Z') ||
               (!iscntrl(key) && key < 128)  ) {
-            s = (char)key;
-            input.append(s);
+            m.input.push_back(key);
         } else if (key == CTRL_KEY('d')) {
-            if (input.size() == 0) {
-                s = (char)CTRL_KEY('d');
-                input.append(s);
+            if (m.input.size() == 0) {
+                m.input.push_back(CTRL_KEY('d'));
                 break;
             }
-        }
-        switch (key) {
-            case Key::BACKSPACE: input = input.substr(0, input.size()-1); break;
-            case Key::ARROW_LEFT: col--; break;
-            case Key::ARROW_RIGHT: col++; break;
+        } else {
+            switch (key) {
+                case Key::BACKSPACE:
+                    m.input = m.input.substr(0, m.input.size()-1); break;
+                case Key::ARROW_LEFT: col--; break;
+                case Key::ARROW_RIGHT: col++; break;
+            }
         }
 
-        std::cout << render(row, cols, prompt_string, input) << std::flush;
+        std::cout << render(m, row, cols) << std::flush;
     }
     std::cout << "\n" << std::flush;
-    return input;
+    return m.input;
 }
 
 int main() {
