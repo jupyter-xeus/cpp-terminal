33a5a764cf906affe796b0023b76ebe966aaebb0
Ondřej Čertík <ondrej@certik.us>
2020-09-11 13:27:15 -0600

    Add a standalone test

---
+++ test-standalone/colors.cpp
@@ -0,0 +1,33 @@
+#include <cpp-terminal/terminal.h>
+
+using Term::Terminal;
+using Term::color;
+using Term::fg;
+using Term::bg;
+using Term::style;
+
+int main() {
+    try {
+        Terminal term;
+        if (term.is_stdout_a_tty()) {
+            std::cout << "Standard output is attached to a terminal."
+                << std::endl;
+        } else {
+            std::cout << "Standard output is not attached to a terminal."
+                << std::endl;
+        }
+        std::string text = "Some text with "
+            + color(fg::red) + color(bg::green) + "red on green"
+            + color(bg::reset) + color(fg::reset) + " and some "
+            + color(style::bold) + "bold text" + color(style::reset) + ".\n";
+        text += "Unicode works too: originally written by Ondřej Čertík.";
+        std::cout << text << std::endl;
+    } catch(const std::runtime_error& re) {
+        std::cerr << "Runtime error: " << re.what() << std::endl;
+        return 2;
+    } catch(...) {
+        std::cerr << "Unknown error." << std::endl;
+        return 1;
+    }
+    return 0;
+}
