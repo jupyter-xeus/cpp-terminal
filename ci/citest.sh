b0b2e1352aa9e6ec5005517c0669fff9395ccb70
damon <mcwertgamingQgmail.com>
2021-02-07 23:26:43 +0000

    move to . as local directory, because 'pwd' is not safe

--- ci/citest.sh
+++ ci/citest.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
+cmake -DCMAKE_INSTALL_PREFIX=./inst .
 cmake --build . --config Release
 cmake --install . --config Release
 ctest --output-on-failure
