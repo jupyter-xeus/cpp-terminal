2e142f512a8fa9db3737577e2df715b5765e083d
Damon Leven <mcwertgaming@gmail.com>
2021-02-24 09:56:47 +0000

    move tests into tests/

--- ci/citest.sh
+++ ci/citest.sh
@@ -7,6 +7,6 @@ cmake --build . --config Release
 cmake --install . --config Release
 ctest --output-on-failure
 
-cd test-standalone
+cd test/test-standalone
 cmake -DCMAKE_PREFIX_PATH=../inst .
 cmake --build . --config Release
