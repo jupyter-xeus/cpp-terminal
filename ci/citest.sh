89c5565b7a94284cca46adf7c08f88c58ae5b14a
Damon Leven <mcwertgaming@gmail.com>
2021-02-28 20:00:39 +0000

    fixed path

--- ci/citest.sh
+++ ci/citest.sh
@@ -7,6 +7,6 @@ cmake --build . --config Release
 cmake --install . --config Release
 ctest --output-on-failure
 
-cd test/test-standalone
+cd tests/test-standalone
 cmake -DCMAKE_PREFIX_PATH=../inst .
 cmake --build . --config Release
