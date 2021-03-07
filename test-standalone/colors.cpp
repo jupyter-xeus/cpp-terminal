f96a1b423aad46cc6974f92113cc7f958f4e3c38
Damon Leven <mcwertgaming@gmail.com>
2021-03-07 21:46:19 +0000

    copied new colors.cpp

--- test-standalone/colors.cpp
+++ test-standalone/colors.cpp
@@ -1,15 +1,20 @@
 #include <cpp-terminal/terminal.h>
+#include <cpp-terminal/version.h>
 
 using Term::Terminal;
 using Term::color;
+using Term::color24;
 using Term::fg;
 using Term::bg;
 using Term::style;
 
 int main() {
+    std::cout << "Running cpp-terminal version: " 
+                << CPP_TERMINAL_VERSION_COMPLETE
+                << std::endl;
     try {
         Terminal term;
-        if (term.is_stdout_a_tty()) {
+        if (Terminal::is_stdout_a_tty()) {
             std::cout << "Standard output is attached to a terminal."
                 << std::endl;
         } else {
@@ -22,6 +27,46 @@ int main() {
             + color(style::bold) + "bold text" + color(style::reset) + ".\n";
         text += "Unicode works too: originally written by Ondřej Čertík.";
         std::cout << text << std::endl;
+
+        std::string rgb_text = "Some Text in "
+            + color24(255, 0, 0, true) + 'R'
+            + color24(0, 255, 0, true) + 'G'
+            + color24(0, 0, 255, true) + 'B'
+            + color(fg::reset);
+
+        std::cout << rgb_text << std::endl;
+
+        std::cout << "A color chart: \n";
+
+        for (unsigned int i = 0; i <= 255; i += 3)
+        {
+            std::cout << color24(i, 0, 0, false)
+                      << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+        for (unsigned int i = 0; i <= 255; i += 3)
+        {
+            std::cout << color24(0, i, 0, false)
+                      << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+        for (unsigned int i = 0; i <= 255; i += 3)
+        {
+            std::cout << color24(0, 0, i, false)
+                      << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+        for (unsigned int i = 0; i <= 255; i += 3)
+        {
+            std::cout << color24(i, i, i, false)
+                      << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+
     } catch(const std::runtime_error& re) {
         std::cerr << "Runtime error: " << re.what() << std::endl;
         return 2;
