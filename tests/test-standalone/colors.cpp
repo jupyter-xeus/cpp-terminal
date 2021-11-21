64b5dcbae592e85981bc35ee109593adbde90bb3
MCWertGaming <dleven@protonmail.com>
2021-11-21 17:06:23 +0100

    updated stand-alone test

--- tests/test-standalone/colors.cpp
+++ tests/test-standalone/colors.cpp
@@ -1,32 +1,69 @@
 #include <cpp-terminal/terminal.h>
+#include <cpp-terminal/version.h>
+#include <cpp-terminal/base.hpp>
 #include <iostream>
 
-using Term::Terminal;
-using Term::color;
-using Term::fg;
 using Term::bg;
+using Term::color;
+using Term::color24_bg;
+using Term::color24_fg;
+using Term::fg;
 using Term::style;
+using Term::Terminal;
 
 int main() {
+    std::cout << "Running cpp-terminal version: "
+              << CPP_TERMINAL_VERSION_COMPLETE << std::endl;
     try {
         Terminal term;
-        if (term.is_stdout_a_tty()) {
+        if (Terminal::is_stdout_a_tty()) {
             std::cout << "Standard output is attached to a terminal."
-                << std::endl;
+                      << std::endl;
         } else {
             std::cout << "Standard output is not attached to a terminal."
-                << std::endl;
+                      << std::endl;
         }
-        std::string text = "Some text with "
-            + color(fg::red) + color(bg::green) + "red on green"
-            + color(bg::reset) + color(fg::reset) + " and some "
-            + color(style::bold) + "bold text" + color(style::reset) + ".\n";
+        std::string text = "Some text with " + color(fg::red) +
+                           color(bg::green) + "red on green" +
+                           color(bg::reset) + color(fg::reset) + " and some " +
+                           color(style::bold) + "bold text" +
+                           color(style::reset) + ".\n";
         text += "Unicode works too: originally written by Ondřej Čertík.";
         std::cout << text << std::endl;
-    } catch(const std::runtime_error& re) {
+
+        std::string rgb_text = "Some Text in " + color24_fg(255, 0, 0) + 'R' +
+                               color24_fg(0, 255, 0) + 'G' +
+                               color24_fg(0, 0, 255) + 'B' + color(fg::reset);
+
+        std::cout << rgb_text << std::endl;
+
+        std::cout << "A color chart: \n";
+
+        for (unsigned int i = 0; i <= 255; i += 3) {
+            std::cout << color24_bg(i, 0, 0) << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+        for (unsigned int i = 0; i <= 255; i += 3) {
+            std::cout << color24_bg(0, i, 0) << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+        for (unsigned int i = 0; i <= 255; i += 3) {
+            std::cout << color24_bg(0, 0, i) << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+        for (unsigned int i = 0; i <= 255; i += 3) {
+            std::cout << color24_bg(i, i, i) << " "
+                      << "\033[0m";
+        }
+        std::cout << "\n";
+
+    } catch (const std::runtime_error& re) {
         std::cerr << "Runtime error: " << re.what() << std::endl;
         return 2;
-    } catch(...) {
+    } catch (...) {
         std::cerr << "Unknown error." << std::endl;
         return 1;
     }
