c8e5a97f80c10f29fa3f696ca62da029a6cd0d04
Damon Leven <dleven@protonmail.com>
2022-05-29 21:29:07 +0200

    fixed binary file paths

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,10 +1,10 @@
 echo "running example programs"
-examples\Release\colors.exe
-tests\test_terminal.exe
-examples\read_stdin.exe
+.\examples\Release\colors.exe
+.\tests\Release\test_terminal.exe
+.\examples\Release\read_stdin.exe
 
 echo "testing read_stind"
-if ("test" examples\read_stdin -ne "Input from stdin: test") {
+if ((echo test | .\examples\Release\read_stdin.exe) -ne "Input from stdin: test") {
     echo "read_stdin gave wrong output"
     exit 1
 }
