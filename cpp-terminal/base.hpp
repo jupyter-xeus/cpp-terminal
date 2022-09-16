e9548279e1bae2302b363f52026ab1c338f1df2a
Damon Leven <dleven@protonmail.com>
2022-09-16 17:25:55 +0200

    added descriptions to the styles

--- cpp-terminal/base.hpp
+++ cpp-terminal/base.hpp
@@ -45,17 +45,27 @@ enum class Color4 : uint8_t {
     // FG: 97, BG: 107
     WHITE_BRIGHT = 67
 };
+/*
+ * Styles for text in the terminal
+ */
 enum class Style : uint8_t {
+    // resets all attributes (styles and colors)
     RESET = 0,
+    // Thick text font
     BOLD = 1,
+    // lighter, slimmer text font
     DIM = 2,
+    // slightly bend text font
     ITALIC = 3,
+    // draws a line below the text
     UNDERLINE = 4,
     BLINK = 5,
     BLINK_RAPID = 6,
     REVERSED = 7,
     CONCEAL = 8,
+    // strikes through the text, mostly supported
     CROSSED = 9,
+    // draws a line over the text, barely supported
     OVERLINE = 53
 };
 
