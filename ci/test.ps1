148eaa4c4640d6c3aa66284c11fe8e2106f17d5d
flagarde <flagarde.programs@pm.me>
2022-09-17 15:32:00 +0200

    Try to fix ci

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,9 +1,9 @@
 echo "running example programs"
-.\examples\Release\colors.exe
-.\examples\Release\read_stdin.exe
+./examples/Release/colors.exe
+./examples/Release/read_stdin.exe
 
 echo "testing read_stind"
-if ((echo test | .\examples\Release\read_stdin.exe) -ne "Input from stdin: test") {
+if ((echo test | ./examples/Release/read_stdin.exe) -ne "Input from stdin: test") {
     echo "read_stdin gave wrong output"
     exit 1
 }
