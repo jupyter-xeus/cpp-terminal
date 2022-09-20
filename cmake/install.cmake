56c63a0bd9d69fd61076a388bb078d9bfa7867f4
flagarde <flagarde.programs@pm.me>
2022-09-20 19:02:18 +0200

    Fix

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,32 +1,9 @@
-function(install_library TARGET_LIB_NAME)
-
-install(TARGETS ${TARGET_LIB_NAME} EXPORT ${TARGET_LIB_NAME}Targets
-    RUNTIME DESTINATION bin
-    LIBRARY DESTINATION lib
-    ARCHIVE DESTINATION lib
-    PUBLIC_HEADER DESTINATION include/${TARGET_LIB_NAME}
-)
-include(CMakePackageConfigHelpers)
-write_basic_package_version_file(
-    "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_LIB_NAME}/${TARGET_LIB_NAME}ConfigVersion.cmake"
-    VERSION ${CPPTERMINAL_VERSION}
-    COMPATIBILITY AnyNewerVersion
-)
-export(EXPORT ${TARGET_LIB_NAME}Targets
-    FILE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_LIB_NAME}/${TARGET_LIB_NAME}Targets.cmake"
-)
-set(ConfigPackageLocation lib/cmake/${TARGET_LIB_NAME})
-configure_file(${TARGET_LIB_NAME}Config.cmake.in
-    "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_LIB_NAME}/${TARGET_LIB_NAME}Config.cmake"
-)
-install(
-    FILES
-    "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_LIB_NAME}/${TARGET_LIB_NAME}Config.cmake"
-    "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_LIB_NAME}/${TARGET_LIB_NAME}ConfigVersion.cmake"
-    "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_LIB_NAME}/${TARGET_LIB_NAME}Targets.cmake"
-    DESTINATION
-    ${ConfigPackageLocation}
-    COMPONENT
-    Devel
-)    
-endfunction(install_library TARGET_LIB_NAME)
+function(install_library)
+  include(GNUInstallDirs)
+  include(CMakePackageConfigHelpers)
+  configure_package_config_file("${PROJECT_SOURCE_DIR}/cmake/cpp-terminalConfig.cmake.in" "${PROJECT_BINARY_DIR}/cmake/cpp-terminalConfig.cmake" INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/cpp-terminal")
+  write_basic_package_version_file("${CMAKE_CURRENT_BINARY_DIR}/cpp-terminalConfigVersion.cmake" COMPATIBILITY AnyNewerVersion)
+  install(FILES "${PROJECT_BINARY_DIR}/cmake/cpp-terminalConfig.cmake" "${CMAKE_CURRENT_BINARY_DIR}/cpp-terminalConfigVersion.cmake" DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/cpp-terminal")
+  install(TARGETS cpp-terminal EXPORT cpp-terminalTargets LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}" ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}" RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}" PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/cpp-terminal" PRIVATE_HEADER DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/cpp-terminal/private")
+  install(EXPORT cpp-terminalTargets FILE  cpp-terminalTargets.cmake NAMESPACE cpp-terminal:: DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/cpp-terminal")
+endfunction()
