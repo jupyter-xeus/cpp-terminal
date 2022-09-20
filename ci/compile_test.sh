1fd7e7f032eead2d1c5399d92495695fddb1ca66
flagarde <flagarde.programs@pm.me>
2022-09-20 20:16:48 +0200

    Fix

--- ci/compile_test.sh
+++ ci/compile_test.sh
@@ -6,7 +6,3 @@ cmake -DCMAKE_INSTALL_PREFIX=./install .
 cmake --build . --config Release
 cmake --install . --config Release
 ctest --output-on-failure
-
-cd tests/test-standalone
-cmake -DCMAKE_PREFIX_PATH=../../install .
-cmake --build . --config Release
