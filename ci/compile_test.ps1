2ffa1a6a692a7f650e622a206d27ae1cf2882581
flagarde <flagarde.programs@pm.me>
2022-09-20 20:43:56 +0200

    Fix

--- ci/compile_test.ps1
+++ ci/compile_test.ps1
@@ -1,11 +1,4 @@
-#cmake -DCMAKE_INSTALL_PREFIX=.\inst .
 cmake -DCMAKE_INSTALL_PREFIX=./install .
-cmake --build . --config Release
-cmake --install . --config Release
+cmake --build .
+cmake --install .
 ctest --output-on-failure
-
-# Install on windows is complicated and not like on linux
-#cd tests\test-standalone
-#cmake -DCMAKE_PREFIX_PATH=%cd%\..\inst .
-#cmake --build . --config Release
-#cd
