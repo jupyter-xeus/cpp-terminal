0a2ab58fb75a2eacf1aced04955546c1ab9144b1
flagarde <flagarde.programs@pm.me>
2022-11-28 09:06:10 +0100

    Correct examples.ps1

--- ci/examples.ps1
+++ ci/examples.ps1
@@ -1,9 +1,10 @@
 echo "running example programs"
-.\examples\install\bin\colors.exe
-.\examples\install\bin\read_stdin.exe
+.\examples\Release\colors.exe
+.\tests\Release\test_terminal.exe
+.\examples\Release\read_stdin.exe
 
 echo "testing read_stind"
-if ((echo test | .\examples\install\bin\read_stdin.exe) -ne "Input from stdin: test") {
+if ((echo test | .\examples\Release\read_stdin.exe) -ne "Input from stdin: test") {
     echo "read_stdin gave wrong output"
     exit 1
 }
