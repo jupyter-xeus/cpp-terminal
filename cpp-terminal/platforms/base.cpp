615f24e3c6d1ead1ce5298c81fab5afc7d09dce7
flagarde <flagarde.programs@pm.me>
2023-03-18 10:21:34 +0800

    Suppress platform.* and clean get_size()

--- cpp-terminal/platforms/base.cpp
+++ cpp-terminal/platforms/base.cpp
@@ -11,11 +11,12 @@
 std::pair<std::size_t, std::size_t> Term::get_size()
 {
 #ifdef _WIN32
-  static HANDLE              hConOut{CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)};
-  CONSOLE_SCREEN_BUFFER_INFO inf;
-  if(GetConsoleScreenBufferInfo(hConOut, &inf)) return {inf.srWindow.Bottom - inf.srWindow.Top + 1, inf.srWindow.Right - inf.srWindow.Left + 1};
-  else
-    return {0, 0};
+  static HANDLE                       hConOut{CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)};
+  CONSOLE_SCREEN_BUFFER_INFO          inf;
+  std::pair<std::size_t, std::size_t> ret{0, 0};
+  if(GetConsoleScreenBufferInfo(hConOut, &inf)) { ret = {inf.srWindow.Bottom - inf.srWindow.Top + 1, inf.srWindow.Right - inf.srWindow.Left + 1}; }
+  CloseHandle(hConOut);
+  return ret;
 #else
   struct winsize ws
   {
