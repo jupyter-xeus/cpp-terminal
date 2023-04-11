535efc60dd53506d5923b58702524bc36b398487
flagarde <flagarde.programs@pm.me>
2023-04-11 19:55:51 +0800

    Nifty counter

--- examples/minimal.cpp
+++ examples/minimal.cpp
@@ -1,3 +1,10 @@
 #include "cpp-terminal/io.hpp"
 
-int main() { Term::terminal; }
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+  Term::cout << "This should be printed only in terminal" << std::endl;
+  std::cout << "This should be printed in terminal if stdout is not redirected" << std::endl;
+}
