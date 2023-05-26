6ae34c973db151341ec933a50c36b5893a5ee678
flagarde <flagarde.programs@pm.me>
2023-05-26 13:06:06 +0200

    Update env.hpp

--- cpp-terminal/platforms/env.hpp
+++ cpp-terminal/platforms/env.hpp
@@ -10,15 +10,15 @@ namespace Term
 namespace Private
 {
 
- /**
- * @brief Value of an environment variables.
- *
- * @param env environment variable.
- * @return std::pair<bool, std::string> with bool:true if the environment variable is set and std::string to the value of environment variable.
- * @warning Internal use only.
- */
+/**
+* @brief Value of an environment variables.
+*
+* @param env environment variable.
+* @return std::pair<bool, std::string> with bool:true if the environment variable is set and std::string to the value of environment variable.
+* @warning Internal use only.
+*/
 std::pair<bool, std::string> getenv(const std::string& env);
 
-}
+} // namespace Private
 
 }  // namespace Term
