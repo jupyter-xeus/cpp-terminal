1ab4e13ae417017abd8f1230b5870100afdf78c9
Tobias Wallner <tobias.wallner1@gmx.net>
2023-07-21 19:16:28 +0200

    added the const qualifier to methods that do not change the object

--- cpp-terminal/key.cpp
+++ cpp-terminal/key.cpp
@@ -2,7 +2,7 @@
 
 #include "cpp-terminal/platforms/conversion.hpp"
 
-std::string Term::Key::str()
+std::string Term::Key::str() const
 {
   std::string ret;
   if(m_value >= 0x10FFFF) return std::string();
@@ -15,23 +15,23 @@ std::string Term::Key::str()
 
 Term::Key::Key(const Term::Key::Value& value) : m_value(value) {}
 
-Term::Key::operator Term::Key::Value() { return m_value; }
+Term::Key::operator Term::Key::Value()  const { return m_value; }
 
-bool Term::Key::isASCII()
+bool Term::Key::isASCII() const
 {
   if(m_value >= NUL && m_value <= DEL) return true;
   else
     return false;
 }
 
-bool Term::Key::isExtendedASCII()
+bool Term::Key::isExtendedASCII() const
 {
   if(m_value >= NUL && m_value <= 255) return true;
   else
     return false;
 }
 
-bool Term::Key::isCTRL()
+bool Term::Key::isCTRL() const
 {
   // Need to suppress the TAB etc...
   if(iscntrl() && m_value != Key::BACKSPACE && m_value != Key::TAB && m_value != ESC && m_value != ENTER && m_value != DEL) return true;
@@ -39,98 +39,98 @@ bool Term::Key::isCTRL()
     return false;
 }
 
-bool Term::Key::isALT()
+bool Term::Key::isALT() const
 {
   if(m_value >= Key::ALT && Key::ALT < (Key::ALT << 1)) return true;
   else
     return false;
 }
 
-bool Term::Key::empty()
+bool Term::Key::empty() const
 {
   if(m_value == NO_KEY) return true;
   else
     return false;
 }
 
-bool Term::Key::iscntrl()
+bool Term::Key::iscntrl() const
 {
   if((m_value >= NUL && m_value <= CTRL_UNDERSCORE) || m_value == DEL) return true;
   else
     return false;
 }
 
-bool Term::Key::isblank()
+bool Term::Key::isblank() const
 {
   if(m_value == TAB || m_value == SPACE) return true;
   else
     return false;
 }
 
-bool Term::Key::isspace()
+bool Term::Key::isspace() const
 {
   if(isblank() || (m_value >= CTRL_J && m_value <= ENTER)) return true;
   else
     return false;
 }
 
-bool Term::Key::isupper()
+bool Term::Key::isupper() const
 {
   if(m_value >= A && m_value <= Z) return true;
   else
     return false;
 }
 
-bool Term::Key::islower()
+bool Term::Key::islower() const
 {
   if(m_value >= a && m_value <= z) return true;
   else
     return false;
 }
 
-bool Term::Key::isalpha()
+bool Term::Key::isalpha() const
 {
   if(isupper() || islower()) return true;
   else
     return false;
 }
 
-bool Term::Key::isdigit()
+bool Term::Key::isdigit() const
 {
   if(m_value >= ZERO && m_value <= NINE) return true;
   else
     return false;
 }
 
-bool Term::Key::isxdigit()
+bool Term::Key::isxdigit() const
 {
   if(isdigit() || (m_value >= A && m_value <= F) || (m_value >= a && m_value <= f)) return true;
   else
     return false;
 }
 
-bool Term::Key::isalnum()
+bool Term::Key::isalnum() const
 {
   if(isdigit() || isalpha()) return true;
   else
     return false;
 }
 
-bool Term::Key::ispunct()
+bool Term::Key::ispunct() const
 {
   if((m_value >= EXCLAMATION_MARK && m_value <= SLASH) || (m_value >= COLON && m_value <= AROBASE) || (m_value >= OPEN_BRACKET && m_value <= GRAVE_ACCENT) || (m_value >= OPEN_BRACE && m_value <= TILDE)) return true;
   else
     return false;
 }
 
-bool Term::Key::isgraph()
+bool Term::Key::isgraph() const
 {
   if(isalnum() || ispunct()) return true;
   else
     return false;
 }
 
-bool Term::Key::isprint()
+bool Term::Key::isprint() const
 {
   if(isgraph() || m_value == SPACE) return true;
   else
