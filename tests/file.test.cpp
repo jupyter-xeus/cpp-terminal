9732206b92b1ce40068a3a870ed2d2ee75984869
flagarde <flagarde.programs@pm.me>
2023-07-23 05:17:28 +0800

    fix

--- tests/file.test.cpp
+++ tests/file.test.cpp
@@ -7,7 +7,9 @@
 
 TEST_CASE("Test platform/file.hpp")
 {
+#if !defined(__MINGW32__)
   Term::Private::m_fileInitializer.init();
   Term::Private::out.write("Good !\n");
   std::fprintf(Term::Private::out.file(), "Good !\n");
+#endif
 }
