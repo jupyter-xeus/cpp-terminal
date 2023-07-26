156b52834821c79cd8b9b080289dd6771734f183
flagarde <flagarde.programs@pm.me>
2023-07-26 20:26:08 +0800

    Update terminfo.cpp

--- cpp-terminal/platforms/terminfo.cpp
+++ cpp-terminal/platforms/terminfo.cpp
@@ -45,12 +45,12 @@ void Term::Terminfo::setLegacy()
   else
   {
     DWORD dwOriginalOutMode{0};
-    Term::Private::m_fileInitializer.initialize();  //Just in case
-    GetConsoleMode(Private::std_cout.getHandler(), &dwOriginalOutMode);
-    if(!SetConsoleMode(Private::std_cout.getHandler(), dwOriginalOutMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING)) m_legacy = true;
+    Term::Private::m_fileInitializer.init();  //Just in case
+    GetConsoleMode(Private::out.handle(), &dwOriginalOutMode);
+    if(!SetConsoleMode(Private::out.handle(), dwOriginalOutMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING)) m_legacy = true;
     else
     {
-      SetConsoleMode(Private::std_cout.getHandler(), dwOriginalOutMode);
+      SetConsoleMode(Private::out.handle(), dwOriginalOutMode);
       m_legacy = false;
     }
   }
