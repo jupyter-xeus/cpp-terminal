817cdcb80428bff0a96b515af5265447e9af1ee5
flagarde <flagarde.programs@pm.me>
2023-07-28 04:00:12 +0800

    linux fix

--- cpp-terminal/platforms/terminfo.cpp
+++ cpp-terminal/platforms/terminfo.cpp
@@ -98,6 +98,8 @@ void Term::Terminfo::setColorMode()
   else if(isLegacy())
     m_colorMode = Term::Terminfo::ColorMode::Bit4;
 #endif
+  else
+    m_colorMode = Term::Terminfo::ColorMode::Bit24;
   std::string colorterm = Private::getenv("COLORTERM").second;
   if((colorterm == "truecolor" || colorterm == "24bit") && m_colorMode != ColorMode::Unset) m_colorMode = Term::Terminfo::ColorMode::Bit24;
 }
