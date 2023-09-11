a3ae7ac4fc0be88111a2d0ec054ecbae5fb2dcbd
flagarde <flagarde.programs@pm.me>
2023-09-11 14:10:04 +0800

    warning

--- cpp-terminal/platforms/conversion.cpp
+++ cpp-terminal/platforms/conversion.cpp
@@ -14,8 +14,8 @@ namespace Private
 #if defined(_WIN32)
 std::string to_utf8(LPCWCH utf16Str)
 {
-  int         size_needed = WideCharToMultiByte(CP_UTF8, 0, utf16Str, -1, nullptr, 0, nullptr, nullptr);
-  std::string ret(size_needed, '\0');
+  int         size_needed{WideCharToMultiByte(CP_UTF8, 0, utf16Str, -1, nullptr, 0, nullptr, nullptr)};
+  std::string ret(static_cast<std::size_t>(size_needed), '\0');
   WideCharToMultiByte(CP_UTF8, 0, utf16Str, wcslen(utf16Str), &ret[0], static_cast<int>(ret.size()), nullptr, nullptr);
   return ret.c_str();
 }
