789153056d9cc5b57f419f4ee572f7630a68f1c9
flagarde <flagarde.programs@pm.me>
2023-09-25 10:09:04 +0200

    Install libs for easier examples

--- cpp-terminal/mouse.cpp
+++ cpp-terminal/mouse.cpp
@@ -1,7 +1,5 @@
 #include "cpp-terminal/mouse.hpp"
 
-Term::Button::Button(const Term::Button::Type& type, const Term::Button::Action& action) : m_type(type), m_action(action){};
-
 Term::Button::Action Term::Button::action() { return m_action; }
 
 Term::Button::Type Term::Button::type() { return m_type; }
@@ -11,8 +9,6 @@ bool Term::Mouse::has(const Term::Button::Type& type, const Term::Button::Action
 std::array<Term::Button, 11> Term::Mouse::getButtons() const { return m_buttons; }
 
 bool Term::Mouse::has(const Term::Button::Type& type) { return m_buttons[static_cast<std::size_t>(type)].action() != Term::Button::Action::None; }
-
-Term::Mouse::Mouse(const std::array<Term::Button, 11>& buttons, const std::uint16_t& row, const std::uint16_t& column) : m_buttons(buttons), m_row(row), m_column(column) {}
 
 std::size_t Term::Mouse::row() { return static_cast<std::size_t>(m_row); }
 
