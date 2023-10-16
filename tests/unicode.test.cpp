7f6ac453ab65573131ca9fa8b416fa151bf10ca4
flagarde <flagarde.programs@pm.me>
2023-10-17 00:24:37 +0800

    Fix test

--- tests/unicode.test.cpp
+++ tests/unicode.test.cpp
@@ -19,14 +19,14 @@ TEST_CASE("to_narrow")
 {
   std::wstring in(L"Hello World; 你好; おはようございます; בוקר טוב");
   std::string  out{Term::Private::to_narrow(in)};
-  CHECK(out == std::string(u8"Hello World; 你好; おはようございます; בוקר טוב"));
+  CHECK(out == (const char*)(u8"Hello World; 你好; おはようございます; בוקר טוב"));
 }
 #endif
 
 #if defined(_WIN32)
 TEST_CASE("to_wide")
 {
-  std::string in(u8"∮ E⋅da = Q,  n → ∞, ∑ f(i) = ∏ g(i)γνωρίζω ἀπὸ τὴν ὄψηდარგებში⠝⠁⠊⠇ ⠁⠎ ⠹⠑ ⠙როგორიცააᚻ∂∈ℝ∧∪≡∞ ↑↗↨↻⇣ ┐┼╔╘░►☺♀ ﬁ�⑀₂ἠḂᛖᛒᚢᛞᛖразличных\tопеฮั่นเสื่อมโทรมแማደሪያ የለው፥ ግንድ ይዞ ይዞራል።\n");  // Some multi-language charabia
+  std::string in((const char*)(u8"∮ E⋅da = Q,  n → ∞, ∑ f(i) = ∏ g(i)γνωρίζω ἀπὸ τὴν ὄψηდარგებში⠝⠁⠊⠇ ⠁⠎ ⠹⠑ ⠙როგორიცააᚻ∂∈ℝ∧∪≡∞ ↑↗↨↻⇣ ┐┼╔╘░►☺♀ ﬁ�⑀₂ἠḂᛖᛒᚢᛞᛖразличных\tопеฮั่นเสื่อมโทรมแማደሪያ የለው፥ ግንድ ይዞ ይዞራል።\n"));  // Some multi-language charabia
   std::wstring out{Term::Private::to_wide(in)};
   CHECK(out == L"∮ E⋅da = Q,  n → ∞, ∑ f(i) = ∏ g(i)γνωρίζω ἀπὸ τὴν ὄψηდარგებში⠝⠁⠊⠇ ⠁⠎ ⠹⠑ ⠙როგორიცააᚻ∂∈ℝ∧∪≡∞ ↑↗↨↻⇣ ┐┼╔╘░►☺♀ ﬁ�⑀₂ἠḂᛖᛒᚢᛞᛖразличных\tопеฮั่นเสื่อมโทรมแማደሪያ የለው፥ ግንድ ይዞ ይዞራል።\n");
 }
