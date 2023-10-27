25e3f142d6ff906c18ec2244d63696db2e48045f
flagarde <flagarde.programs@pm.me>
2023-10-27 17:06:50 +0200

    fix (#324)

--- cpp-terminal/tty.hpp
+++ cpp-terminal/tty.hpp
@@ -12,28 +12,28 @@
 namespace Term
 {
 
-/**
- * @brief Check if \b stdin is a \b tty.
- *
- * @return true : \b stdin is a \b tty.
- * @return false : \b stdin is not a \b tty.
- */
+///
+/// @brief Check if \b stdin is a \b tty.
+///
+/// @return true : \b stdin is a \b tty.
+/// @return false : \b stdin is not a \b tty.
+///
 bool is_stdin_a_tty();
 
-/**
- * @brief Check if \b stdout is a \b tty.
- *
- * @return true : \b stdout is a \b tty.
- * @return false : \b stdout is not a \b tty.
- */
+///
+/// @brief Check if \b stdout is a \b tty.
+///
+/// @return true : \b stdout is a \b tty.
+/// @return false : \b stdout is not a \b tty.
+///
 bool is_stdout_a_tty();
 
-/**
- * @brief Check if \b stderr is a \b tty.
- *
- * @return true : \b stderr is a \b tty.
- * @return false : \b stderr is not a \b tty.
- */
+///
+/// @brief Check if \b stderr is a \b tty.
+///
+/// @return true : \b stderr is a \b tty.
+/// @return false : \b stderr is not a \b tty.
+///
 bool is_stderr_a_tty();
 
 }  // namespace Term
