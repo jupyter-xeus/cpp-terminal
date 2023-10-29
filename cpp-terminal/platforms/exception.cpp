0927b0e5b8153869cf6f144a19ef00e78962f94f
flagarde <flagarde.programs@pm.me>
2023-10-30 01:29:16 +0800

    fix

--- cpp-terminal/platforms/exception.cpp
+++ cpp-terminal/platforms/exception.cpp
@@ -107,3 +107,26 @@ void Term::Private::ErrnoException::build_what()
   m_what = "errno " + std::to_string(m_code) + ": " + m_message;
   if(!m_context.empty()) m_what += +" [" + m_context + "]";
 }
+
+Term::Exception::Exception(const std::string& message) : m_message(message) { build_what(); }
+
+Term::Exception::Exception(const std::int64_t& code, const std::string& message) : m_code(code), m_message(message) { build_what(); }
+
+Term::Exception::Exception(const std::int64_t& code) : m_code(code) {}
+
+Term::Exception::~Exception(){};
+
+const char* Term::Exception::what() const noexcept { return m_what.c_str(); }
+
+void Term::Exception::build_what()
+{
+  if(m_code == 0) { m_what = m_message; }
+  else
+    m_what = "error " + std::to_string(m_code) + ": " + m_message;
+}
+
+std::int64_t Term::Exception::code() const noexcept { return m_code; }
+
+std::string Term::Exception::message() const noexcept { return m_message; }
+
+std::string Term::Exception::context() const noexcept { return m_context; }
