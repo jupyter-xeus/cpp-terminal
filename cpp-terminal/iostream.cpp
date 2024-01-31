a7f469c28ec5c720cc886654d124f65c3600ab99
tlane-jlg <93147696+tlane-jlg@users.noreply.github.com>
2024-01-31 16:35:40 -0500

    Fixed typo in cin_buffer size (#350)

--- cpp-terminal/iostream.cpp
+++ cpp-terminal/iostream.cpp
@@ -16,7 +16,7 @@ namespace
 std::array<char, sizeof(Term::TOstream)> cout_buffer;  //NOLINT(fuchsia-statically-constructed-objects)
 std::array<char, sizeof(Term::TOstream)> clog_buffer;  //NOLINT(fuchsia-statically-constructed-objects)
 std::array<char, sizeof(Term::TOstream)> cerr_buffer;  //NOLINT(fuchsia-statically-constructed-objects)
-std::array<char, sizeof(Term::TOstream)> cin_buffer;   //NOLINT(fuchsia-statically-constructed-objects)
+std::array<char, sizeof(Term::TIstream)> cin_buffer;   //NOLINT(fuchsia-statically-constructed-objects)
 }  // namespace
 
 Term::TOstream& Term::cout = reinterpret_cast<Term::TOstream&>(cout_buffer);  //NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
